0a4ef187ad9616d3a9ecefeffb62b4ad5797d885
Utkarsh10 <utkarshnagar2000@gmail.com>
2021-02-20 18:50:18 +0530

    Cards bug fixed

--- retrieve.js
+++ retrieve.js
@@ -12,22 +12,23 @@ function getData() {
   db.collection("restaurant")
     .get()
     .then((snapshot) => {
-        setUpGuides(snapshot.docs);
+      setUpGuides(snapshot.docs);
     });
 }
 
 const setUpGuides = (data) => {
-    let html = '';
-    data.forEach(doc => {
-        const guide = doc.data();
-        console.log(guide);
-        const li = `
+  let html = "";
+  data.forEach((doc) => {
+    const guide = doc.data();
+    console.log(guide);
+    const li = `
+    <div class="row">
         <div class="flip">
             <div class="front" style="background-image: url(https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb)">
                <h1 class="text-shadow">${guide.Name}</hi>
             </div>
             <div class="back">
-               <h3>${guide.Name}</h3>
+              
                <p>Incharge: ${guide.personName}</p>
                <p>Capacity to feed ${guide.People} people</p>
                <p>Email Address: ${guide.Email}</p>
@@ -35,12 +36,13 @@ const setUpGuides = (data) => {
                <p>Address :${guide.Address}</p>
             </div>
          </div>
+         </div>
         `;
-        html += li;
-    });
+    html += li;
+  });
 
-    guideList.innerHTML = html;
-}
+  guideList.innerHTML = html;
+};
 
 getInfo.onclick = function () {
   getData();
